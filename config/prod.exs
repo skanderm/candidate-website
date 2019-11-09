ca0de5e4ea007fa580e493f48fb729ca805c22f4
Skander Mzali <skanderm@gmail.com>
2019-11-09 14:19:29 -0500

    Add secret key base

--- config/prod.exs
+++ config/prod.exs
@@ -21,7 +21,8 @@ config :candidate_website, CandidateWebsite.Endpoint,
   ],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
-  server: true
+  server: true,
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
